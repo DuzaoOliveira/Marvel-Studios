5cc4f0fe67ec8a8e20ccc2661db6df03a74dbd3d
Nathan Ferraz <edu.oliblack@hotmail.com>
2022-10-17 17:40:25 -0300

    marvel1

---
+++ marvel/herois-marvel/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from './styles.module.css';
+
+function Pokemons() {
+  <p>teste</p>
+
+  const handleClick = () => {
+    window.location.href = "/";
+  };
+
+}
--- marvel/pages/index.js
+++ marvel/pages/index.js
@@ -1,21 +1,44 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
 
-export default function Home() {
+function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [name, setName] = useState('')
+
+
+  const heroi = (name) => {
+    setIsOpen(true);
+    axios.get(` http://gateway.marvel.com/v1/public/characters?nameStartsWith=iron&ts=1&apikey=06ead66137452ef75685fcdc895a6c0b&hash=2774d42849c52a2ec23f9b2298e41e7a/${name}`)
+      .then((preview) => {
+        setResposta(preview.data);
+      })
+
+  }
+
   return (
-    <><div className={styles.logohome}>
-      <img
-        src='https://www.g33kmania.com/wp-content/uploads/marvel-logo-personnages.jpg'
-        alt="logo pokemon" />
-    </div><>
-        <div className={styles.input}>
-          <input onChange={(e) => setName(e.target.value)} value={'teste'} />
-          <button onClick={() => heroi()}>Buscar</button>
+    <>
+
+      {Object.values(name).map((item) => {
+        <p>{item?.name}</p>;
+      })}
+
+      <div className={styles.logohome}>
+        <img
+          src='https://www.g33kmania.com/wp-content/uploads/marvel-logo-personnages.jpg'
+          alt="logo marvel" />
+      </div>
+      <div className={styles.input}>
+        <input onChange={(e) => setName(e.target.value)} value={name} />
+        <div className={styles.button}>
+          <button onClick={() => window.location.href = "/heroi"}>Buscar</button>
         </div>
-      </></>
+      </div>
+    </>
+  );
 
-
-
-  )
 }
+
+export default Home;
