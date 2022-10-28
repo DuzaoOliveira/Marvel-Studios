d7cb77d7766194faddfeefed925e22bb2c30b9a7
felipepaulino <felipepaulino@frwk.com.br>
2022-10-28 12:56:37 -0300

    alteração

--- marvel/pages/heroes/index.js
+++ marvel/pages/heroes/index.js
@@ -8,16 +8,6 @@ function Mysuperhero() {
   const [valores, setValores] = useState();
   const [nameHeroes, setNameHeroes] = useState();
 
-  // const escolherHero = (name) => {
-  //   axios
-  //     .get(
-  //       `http://gateway.marvel.com/v1/public/characters?${name}&ts=1&apikey=06ead66137452ef75685fcdc895a6c0b&hash=2774d42849c52a2ec23f9b2298e41e7a`,
-  //     )
-  //     .then((preview) => {
-  //       setValores(preview.data);
-  //     })
-  // }
-
   let url
   let nameHero
 
@@ -26,20 +16,19 @@ function Mysuperhero() {
     nameHero = url.split("name=")[1];
   }
 
-  console.log(nameHeroes, 'nameHero')
+  console.log(nameHero, 'nameHero')
 
 
   useEffect(() => {
     axios
       .get(
-        `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameHeroes}&ts=1&apikey=dfdfc06935a1fe33837da6934f7b5373&hash=f5a214e5c63b897dfe0ebc1a1185c936`)
+        `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameHero}&ts=1&apikey=dfdfc06935a1fe33837da6934f7b5373&hash=f5a214e5c63b897dfe0ebc1a1185c936`)
       .then((response) => {
         setResposta(response.data);
-        
       });
   }, [nameHeroes]);
 
-  
+  console.log(resposta, 'ress')
 
   const handleClick = () => {
     window.location.href = "/";
@@ -58,46 +47,23 @@ function Mysuperhero() {
       </>
 
       <div>
-        {console.log(resposta, 'abacaxi')}
         <>
-          {resposta && 
-          
-          resposta?.data?.results?.map((item) => {
-            {console.log(item, 'edu')}
-            return (
-              // eslint-disable-next-line react/jsx-key
-              <div className={styles.card} onClick={() => escolherHero(item)}>
+          {resposta &&
 
-                <div className={styles.nomes}>
-                  <p>{item.name}</p>
+            resposta?.data?.results?.map((item) => {
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <div className={styles.card} onClick={() => escolherHero(item)}>
+
+                  <div className={styles.nomes}>
+                    <p>{item.name}</p>
+                    <p>{item.description}</p>
+                  </div>
                 </div>
-
-                {/* <div className={styles.api}>
-                  <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index + 1}.png`} />
-                </div> */}
-              </div>
-            );
-          })}
+              );
+            })}
         </>
       </div>
-
-      {/* {valores &&
-        <Modal
-          nome={valores?.name}
-          tipo={valores.types && valores?.types[0]?.type?.name}
-          hp={valores.stats && valores?.stats[0].base_stat}
-          ataque={valores.stats && valores?.stats[1].base_stat}
-          defesa={valores.stats && valores?.stats[2].base_stat}
-          ataqueEspecial={valores.stats && valores?.stats[3].base_stat}
-          defesaEspecial={valores.stats && valores?.stats[4].base_stat}
-          velocidade={valores.stats && valores?.stats[5].base_stat}
-          imagem={
-            valores?.sprites?.other["official-artwork"]?.front_default
-          }
-          setIsOpen={setOpenModal}
-          isOpen={openModal}
-        />
-      } */}
     </div>
 
   );
--- marvel/pages/index.js
+++ marvel/pages/index.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
-// import axios from "axios";
 import styles from '../styles/Home.module.css'
 
 function Home() {
-  // const [resposta, setResposta] = useState('')
-  // const [isOpen, setIsOpen] = useState(false);
   const [name, setName] = useState('')
   const [nameHeroes, setNameHeroes] = useState();
 
@@ -21,27 +18,21 @@ function Home() {
         <div className={styles.button}>
           <input onChange={(e) => setNameHeroes(e.target.value)} value={nameHeroes} />
 
-          <button onClick={() =>{
-             setName(name),
-             (window.location.href =  `/heroes?name=${nameHeroes}`)
+          <button onClick={() => {
+            setName(name),
+              (window.location.href = `/heroes?name=${nameHeroes}`)
           }}
           >Buscar</button>
         </div>
       </div>
-
-      {console.log(nameHeroes, 'edu')}
 
       <div className={styles.enter}>
         <button onClick={() => window.location.href = `/heroes?name=${nameHeroes}`}>
           Mysuperhero
         </button>
       </div>
-
     </div>
-
-
   );
-
 }
 
 
